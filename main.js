70674f470f25f4a2caaa986b98acdfeb92570a08
Abhijeet Gund <90125659+Abhigund@users.noreply.github.com>
2024-03-15 23:25:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const greenButton = document.getElementById("callButton");
+const threeButtonsContainer = document.getElementById("three-button-container");
+const endCallButton = document.getElementById("end-call-button");
+
+greenButton.addEventListener("click", () => {
+  threeButtonsContainer.style.display = "flex";
+  greenButton.style.display = "none";
+});
+
+endCallButton.addEventListener("click", () => {
+  threeButtonsContainer.style.display = "none";
+  greenButton.style.display = "flex";
+});
